a15c269b000799655e5ef487ead5ec39b25f1bcb
RanLib20023 <rlibby68@gmail.com>
2024-08-27 10:13:55 -0400

    upload the first project

---
+++ mavenproject1/src/main/java/com/amz/shoppingcart/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.amz.shoppingcart.mavenproject1;
+
+/**
+ *
+ * @author rlibb
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
