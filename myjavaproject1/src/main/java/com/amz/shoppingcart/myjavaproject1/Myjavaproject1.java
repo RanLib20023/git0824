9baae11038241cfa3b28a974f93a3bb1770f29f3
RanLib20023 <rlibby68@gmail.com>
2024-09-03 11:00:52 -0400

    upload first project

---
+++ myjavaproject1/src/main/java/com/amz/shoppingcart/myjavaproject1/Myjavaproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.amz.shoppingcart.myjavaproject1;
+
+/**
+ *
+ * @author rlibb
+ */
+public class Myjavaproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
